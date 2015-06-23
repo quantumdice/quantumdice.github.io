9aa108f0979f5fe98b955769dbceaf8812c4a167
quantumdice <quantumdice9@gmail.com>
2015-06-23 18:00:12 +0800

    Minor fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdNvggTAAAAANd3cn-AD54gQFOYFu4Si3FYSPq0',  // <----- EDIT ME!
-  redirect_uri: 'quantumdice.github.io',
+  redirect_uri: 'https://quantumdice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
